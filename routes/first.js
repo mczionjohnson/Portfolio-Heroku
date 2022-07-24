c8b603fd519887e5bca45e44df2b2c57cf64d72f
mczionjohnson <87995811+mczionjohnson@users.noreply.github.com>
2022-07-24 02:20:44 +0100

    without setHeader set-Cookie with options for fontawesome and bootstrap

--- routes/first.js
+++ routes/first.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
-    res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+    // res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
 
     res.status(200).render('first')
 })
