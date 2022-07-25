c8d6055ab174cf4eb311c96167a1f59d8cdd4d0a
mczionjohnson <87995811+mczionjohnson@users.noreply.github.com>
2022-07-25 01:09:38 +0100

    created and tested responsive.css

--- routes/about.js
+++ routes/about.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
+    res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
 
     res.status(200).render('about')
 })
