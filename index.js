4bd34f57833ce2a447d545714fd005324ac46e1d
mczionjohnson <mczionjohnson@gmail.com>
2024-05-31 09:57:28 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 var cookieParser = require('cookie-parser')
 const expressLayouts = require('express-ejs-layouts')
+const dotenv = require("dotenv")
 
-
+dotenv.config()
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -17,7 +18,6 @@ app.use(cookieParser())
 
 app.use('/', require('./routes/first')) 
 app.use('/project', require('./routes/project')) 
-app.use('/about', require('./routes/about')) 
 
 
 
